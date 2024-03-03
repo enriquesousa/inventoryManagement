b4d16c051de65ab1dfb8694cc552935fa86e6a1d
Enrique <enrique.sousa@gmail.com>
2024-03-02 18:20:27 -0800

    S18. 112. Add - Supplier Page Setup Part 5

--- app/Http/Controllers/Pos/SupplierController.php
+++ app/Http/Controllers/Pos/SupplierController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Supplier;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 
 class SupplierController extends Controller
 {
@@ -22,6 +24,27 @@ class SupplierController extends Controller
         return view('backend.supplier.add_supplier');
     }
 
+
+    // StoreSupplier
+    public function StoreSupplier(Request $request){
+     
+        Supplier::insert([
+            'name' => $request->name,
+            'mobile_no' => $request->mobile_no,
+            'email' => $request->email,
+            'address' => $request->address,
+            'created_by' => Auth::user()->id,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'Proveedor Guardado Correctamente',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('list.supplier')->with($notification);
+
+    }
 
 
 

5b4706c08e27e7fee1c0bb8e26c00771f60f40f2
Enrique <enrique.sousa@gmail.com>
2024-03-10 19:24:52 -0700

    S24. 150. Tablas - Invoice Management Setup Part 2

--- database/migrations/2024_03_10_100709_create_payment_details_table.php
+++ database/migrations/2024_03_10_100709_create_payment_details_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('payment_details', function (Blueprint $table) {
             $table->id();
+
+            $table->integer('invoice_id')->nullable();
+            $table->double('current_paid_amount')->nullable();
+            $table->date('date')->nullable();
+            $table->integer('updated_by')->nullable(); 
+            
             $table->timestamps();
         });
     }

d117a42a8f8dfe0e6dd2b92e0a57deeb8f84e0b4
Enrique <enrique.sousa@gmail.com>
2024-03-19 10:36:17 -0700

    S30. 178. Supplier Wise Stock Report Part 2

--- app/Http/Controllers/Pos/StockController.php
+++ app/Http/Controllers/Pos/StockController.php
@@ -36,6 +36,12 @@ class StockController extends Controller
        return view('backend.stock.supplier_product_wise_report',compact('suppliers','categories'));
     }
 
+    // SupplierWisePdf
+    public function SupplierWisePdf(Request $request){
+        $allData = Product::orderBy('supplier_id','asc')->orderBy('category_id','asc')->where('supplier_id',$request->supplier_id)->get();
+        return view('backend.pdf.supplier_product_wise_report_pdf',compact('allData'));
+    }
+
 
 
 
--- resources/views/backend/stock/supplier_product_wise_report.blade.php
+++ resources/views/backend/stock/supplier_product_wise_report.blade.php
@@ -1,5 +1,9 @@
 @extends('admin.admin_master')
 @section('admin')
+
+    {{-- Jquery CDN Para poder usar JS --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
     <div class="page-content">
         <div class="container-fluid">
 
@@ -24,7 +28,7 @@
                                     <h4 class="card-title">Reporte de <code>Proveedores y Productos</code></h4>
                                     {{-- <h4 class="card-title">Reporte de <strong>Proveedores y Productos</strong></h4>
                                     <p class="card-title-desc">Reporte de <code>Proveedores y Productos</code>.</p> --}}
-                                </div>    
+                                </div>
                                 {{-- <div class="col-6" style="text-align: right;">
                                     <a href="{{ route('stock.report.pdf') }}" target="_blank" class="btn btn-success waves-effect waves-light">
                                         <i class="fa fa-print"></i> 
@@ -37,26 +41,30 @@
                                 <div class="col-md-12 text-center">
 
                                     <strong>Reporte de Proveedores</strong>
-                                    <input type="radio" name="supplier_product_wise" value="supplier_wise" class="search_value">
+                                    <input type="radio" name="supplier_product_wise" value="supplier_wise"
+                                        class="search_value">
                                     &nbsp;&nbsp;
 
                                     <strong>Reporte de Productos</strong>
-                                    <input type="radio" name="supplier_product_wise" value="product_wise" class="search_value">
+                                    <input type="radio" name="supplier_product_wise" value="product_wise"
+                                        class="search_value">
 
                                 </div>
                             </div>
-                            
 
-                            <div class="show_supplier">
-                                <form method="" action="">
+
+                            <div class="show_supplier" style="display: none">
+
+                                <form method="GET" action="{{ route('supplier.wise.pdf') }}" id="myForm" target="_blank">
 
                                     <div class="row">
 
                                         {{-- Seleccionar Proveedor --}}
-                                        <div class="col-sm-8">
+                                        <div class="col-sm-8 form-group">
                                             <label for="">Nombre del Proveedor</label>
-                                            <select name="supplier_id" class="form-select select2" aria-label="Default select example">
-                                                <option selected="">Seleccionar un Proveedor</option>
+                                            <select name="supplier_id" class="form-select select2"
+                                                aria-label="Default select example">
+                                                <option value="">Seleccionar un Proveedor</option>
                                                 @foreach ($suppliers as $item)
                                                     <option value="{{ $item->id }}">{{ $item->name }}</option>
                                                 @endforeach
@@ -71,6 +79,7 @@
                                     </div>
 
                                 </form>
+
                             </div>
 
                         </div>
@@ -81,4 +90,63 @@
         </div> <!-- container-fluid -->
     </div>
     <!-- End Page-content -->
+
+
+    {{-- JS para el manejo de validaciones --}}
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#myForm').validate({
+
+                rules: {
+                    supplier_id: {
+                        required: true,
+                    },
+
+
+                },
+                messages: {
+                    supplier_id: {
+                        required: 'Favor de seleccionar un Proveedor',
+                    },
+
+
+                },
+                errorElement: 'span',
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                },
+            });
+        });
+    </script>
+
+    {{-- JS para mostrar u ocultar el div show_supplier --}}
+    <script type="text/javascript">
+
+        $(document).on('change','.search_value', function(){
+            var search_value = $(this).val();
+            if (search_value == 'supplier_wise') {
+                $('.show_supplier').show();
+            }else{
+                $('.show_supplier').hide();
+            }
+        });
+
+        $(document).on('change','.search_value', function(){
+            var search_value = $(this).val();
+            if (search_value == 'product_wise') {
+                $('.show_product').show();
+            }else{
+                $('.show_product').hide();
+            }
+        });
+
+    </script>
+
 @endsection

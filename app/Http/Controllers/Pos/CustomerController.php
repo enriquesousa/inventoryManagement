83aebe09020d92db818beca4003900b748e0287c
Enrique <enrique.sousa@gmail.com>
2024-03-21 10:02:13 -0700

    S35. 191. Customer Edit Invoice Part 4

--- app/Http/Controllers/Pos/CustomerController.php
+++ app/Http/Controllers/Pos/CustomerController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Customer;
 use App\Models\Payment;
+use App\Models\PaymentDetail;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Carbon;
 use Intervention\Image\ImageManager;
@@ -169,8 +170,39 @@ class CustomerController extends Controller
                 'alert-type' => 'error'
             );
             return redirect()->back()->with($notification);
-            
+
         }else{
+
+            $payment = Payment::where('invoice_id',$invoice_id)->first();
+            $payment_details = new PaymentDetail();
+            $payment->paid_status = $request->paid_status;
+
+            if ($request->paid_status == 'full_paid') {
+
+                $payment->paid_amount = Payment::where('invoice_id',$invoice_id)->first()['paid_amount']+$request->new_paid_amount;
+                $payment->due_amount = '0';
+                $payment_details->current_paid_amount = $request->new_paid_amount;
+
+            } elseif ($request->paid_status == 'partial_paid') {
+
+                $payment->paid_amount = Payment::where('invoice_id',$invoice_id)->first()['paid_amount']+$request->paid_amount;
+                $payment->due_amount = Payment::where('invoice_id',$invoice_id)->first()['due_amount']-$request->paid_amount;
+                $payment_details->current_paid_amount = $request->paid_amount;
+
+            }
+
+            $payment->save();
+
+            $payment_details->invoice_id = $invoice_id;
+            $payment_details->date = date('Y-m-d',strtotime($request->date));
+            $payment_details->updated_by = Auth::user()->id;
+            $payment_details->save();
+
+            $notification = array(
+                'message' => 'Factura actualizada con éxito', 
+                'alert-type' => 'success'
+            );
+            return redirect()->route('credit.customer')->with($notification); 
 
         }  
        
